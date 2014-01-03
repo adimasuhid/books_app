9ed2be6d8dddc0440efaec469ae4c3f4ea0fcecc
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2014-01-03 17:19:47 +0800

    Adds Stuff

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,14 +1,21 @@
 var BookView = Backbone.View.extend({
+    initialize: function(options){
+        this.book = options.book;
+    },
+
     events: {
         "click" : "showMeMore",
     },
 
     className: "lalalala",
 
-    template: _.template("<div class='book' style='border: 1px solid black'><h2>Eloquent Ruby</h2><span>Dave Thomas</span></div>"),
+    template: _.template("<div class='book' style='border: 1px solid black'><h2><%= title %></h2><span><%= author %></span></div>"),
 
     render: function(){
-        $(this.el).html(this.template());
+        $(this.el).html(this.template({
+            title: this.book.get("title"),
+            author: this.book.get("author")
+        }));
 
         return this
     },
@@ -23,22 +30,59 @@ var BookView = Backbone.View.extend({
 });
 
 var BookListView = Backbone.View.extend({
+    initialize: function(options){
+        this.ace = "lallalalaa";
+        this.books = options.books;
+        this.books.on("add", this.render, this);
+    },
+
     el: ".book-holder",
 
     render: function(){
-        for (i=0; i<5; i++){
-            var childBookView = new BookView();
-            $(this.el).append(childBookView.render().el);
-        }
+        var that = this;
+
+        this.clear();
+        this.books.each(function(model){
+            var childBookView = new BookView({book: model});
+            $(that.el).append(childBookView.render().el);
+        });
     },
 
-    initialize: function(){
-        this.childBookView = new BookView();
-        this.ace = "lallalalaa"
-    },
+    clear: function(){
+        $(this.el).empty();
+    }
 
 });
 
-var childBookListView = new BookListView();
+var Book = Backbone.Model.extend({
+    changeTitle: function(title){
+        this.set("title", title);
+    }
+});
+
+var Books = Backbone.Collection.extend({
+    model: Book,
+
+    filterWithAuthor: function(){
+        return this.filter(function(model){ return model.get("author") != null})
+    },
+
+    batchChangeTitle: function(title){
+        this.each(function(model){
+            model.changeTitle(title);
+        });
+    }
+
+});
+
+window.books = new Books([
+        {title: "Book A", author: "Author A"},
+        {title: "Book B", author: "Author B"}
+    ])
+
+var childBookListView = new BookListView({
+    books: books
+});
+
 childBookListView.render();
 
